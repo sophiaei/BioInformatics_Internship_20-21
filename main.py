1b932ced4e7566de0db208c074755f17932218ce
sophiaei <nycsophia@gmail.com>
2020-11-24 22:48:17 +0000

    edited comments

--- main.py
+++ main.py
@@ -22,15 +22,20 @@ def dna_sequence():
   #returns the full sequence.
   return full_sequence
 
-
+#User inputs for the file
 filename = input("File name is: ")
 fileheader = input("Gene Info: ")
 
+#function to convert the fasta string into a real file
 def fasta_maker(filename, fileheader):
   try:
+    #opens file.io
     with open(filename + ".fasta", "w") as spidroin_handle:
+      #the file header
       spidroin_handle.write(">" + fileheader)
+      #the file contents
       spidroin_handle.write(dna_sequence())
+  #bypassing any errors, noting if any happen
   except IOError as err:
     print("theres an error")
   return spidroin_handle
